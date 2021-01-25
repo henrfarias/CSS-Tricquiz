ba87857e95f99e7b1576e4292b4a5dc69a6e6f33
henrfarias <72604637+henrfarias@users.noreply.github.com>
2021-01-25 15:16:15 -0300

    link for github

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/henrfarias" />
+      <GitHubCorner projectUrl="https://github.com/henrfarias/CSS-Tricquiz" />
     </QuizBackground>
   );
 }
