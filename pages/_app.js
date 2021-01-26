0d9ad1b25ff95616eaad22aa75aec08bbf7540cb
henrfarias <72604637+henrfarias@users.noreply.github.com>
2021-01-26 16:32:35 -0300

    adjust meta tag og:image

--- pages/_app.js
+++ pages/_app.js
@@ -37,10 +37,10 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts. gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
-        <meta property="og:image" content="https://drive.google.com/file/d/1XCLABfeO6hp4iViZGH9z4alvPAZQ-iEX/view?usp=sharing" />
+        <meta property="og:image" content="http://drive.google.com/file/d/1XCLABfeO6hp4iViZGH9z4alvPAZQ-iEX/view?usp=sharing" />
         <meta property="og:url" content="https://css-tricquiz-git-main.henrfarias.vercel.app/" />
         <meta property="og:title" content="CSS Tricquiz" />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content="game" />
         <meta property="og:description" content="Teste seus conhecimentos de CSS com este quiz sobre truques mágicos do CSS!" />
         <meta property="og:locale" content="pt_BR" />
       </Head>
